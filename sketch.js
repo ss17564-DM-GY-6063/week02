3b1a472d402a04d9907e25cf35a6a92cf2d2d3eb
shuoshan <ss17564@nyu.edu>
2023-09-13 15:25:08 -0400

    added two ellipsis and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
+    createCanvas(600, 600); //pixels//
+    background(0,255,255); 
 }
 
 function draw() {
+    stroke(255);
+    fill("blue");
+    ellipse(300, 300, 50, 50);
+    fill(255,255,255);
+    ellipse(400, 300, 100, 100);
+    stroke(0);
+    strokeWeight(4);
+    rect(300,300,2200,200);
 }
